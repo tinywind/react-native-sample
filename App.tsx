8867a0340795b54fcc7f25a5a81efe7c5b23ba50
tinywind <tinywind@saerosoft.com>
2023-06-14 17:31:45 +0900

    feats: FlatList

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Button, Linking, RefreshControl, SafeAreaView, ScrollView, ScrollViewComponent, StatusBar, StyleSheet, Text, useColorScheme, useWindowDimensions, View } from 'react-native';
+import { Button, SafeAreaView, StatusBar, StyleSheet, Text, useColorScheme, View, FlatList } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+import debounce from './utils/debounce';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -8,10 +9,10 @@ function App() {
 
   type ItemType = { key: number; value: string };
   const [items, setItems] = useState<ItemType[]>([]);
-  const [refreshing, setRefreshing] = useState(false);
   useEffect(() => init(), []);
   const init = () => setItems(Array.from({ length: 20 }, (a, i) => ({ key: i + 1, value: 'item ' + (i + 1) })));
-  const scrollViewRef = useRef<ScrollView>(null);
+  const ref = useRef<FlatList>(null);
+  let timeoutId: number | undefined;
 
   return (
     <SafeAreaView style={{ ...styles.body, ...backgroundStyle }}>
@@ -20,20 +21,29 @@ function App() {
         title='add'
         onPress={() => {
           setItems(prev => [...prev, { key: prev.length + 1, value: 'new item ' + (prev.length + 1) }]);
-          scrollViewRef.current?.scrollToEnd({ animated: true });
+          timeoutId = debounce(timeoutId, () => ref.current?.scrollToEnd({ animated: true }));
         }}
       />
-      <ScrollView
-        ref={scrollViewRef}
-        style={{ flex: 1, flexDirection: 'column' }}
-        horizontal={false}
-        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={() => (setRefreshing(false), init())} colors={['#ff00ff']} />}>
-        {items.map(e => (
-          <View key={e.key} style={{ backgroundColor: 'grey', justifyContent: 'center', alignItems: 'center', borderBottomWidth: StyleSheet.hairlineWidth, marginBottom: 1 }}>
-            <Text style={{ color: 'white', fontSize: 40, margin: 10 }}>{e.value}</Text>
+      <FlatList
+        ref={ref}
+        horizontal={true}
+        // numColumns={2}
+        keyExtractor={(item, index) => index.toString()}
+        data={items}
+        renderItem={({ item, index }) => (
+          <View
+            key={index}
+            style={{
+              backgroundColor: 'grey',
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderBottomWidth: StyleSheet.hairlineWidth,
+              marginBottom: 1,
+            }}>
+            <Text style={{ color: 'white', fontSize: 40, margin: 10 }}>{item.value}</Text>
           </View>
-        ))}
-      </ScrollView>
+        )}
+      />
     </SafeAreaView>
   );
 }
